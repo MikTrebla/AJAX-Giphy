0e953d59f118ac8dac3ec2c942911a6faa0002c1
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-05-28 21:33:45 -0700

    updated js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -46,9 +46,9 @@ $(document).ready(function () {
             // $('#show-gifs').empty(); 
             for (var i =0; i < results.length; i++) {
                 var newDiv = $('<div class = "col-md-4">');
-                var downloadLink = $('<a>').text('Download GIF');
-                downloadLink.attr('href', results[i].images.fixed_height.url).attr('download', results[i].images.fixed_height.url);
-                var p = $('<p>').html('Title: ' + results[i].title  + '</br>Rating : '+ results[i].rating  );
+                var downloadLink = $('<a>').text('Download GIF');//attempt to create a download GIF link
+                downloadLink.attr('href', results[i].images.original.mp4).attr('download', 'downloadGIF').attr('target', '_blank');
+                var p = $('<p>').html('Title: ' + results[i].title  + '</br>Rating : '+ results[i].rating.toUpperCase()  );
                 var newGif = $('<img>').attr('src', results[i].images.fixed_height_still.url);
                 newGif.attr('data-still', results[i].images.fixed_height_still.url).attr('data-animated', results[i].images.fixed_height.url).attr('data-state', 'still').addClass('gif');
                 newDiv.append(newGif, p, downloadLink);
